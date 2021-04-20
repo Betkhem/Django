8728a99d0c723f6a250ae285fece642859606cae
Betkhem <yaroslav2002roku@gmail.com>
2021-04-21 00:35:05 +0300

    Modified
    Description

--- DJSEC/DJSEC/views.py
+++ DJSEC/DJSEC/views.py
@@ -1,6 +1,14 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def reversed(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1] 
+    Len = len(user_text.split())
+    print(Len)
+    return render(request, 'reversed.html', {'reversedtext': reversed_text, 'usertext': user_text, 'len': Len})
